a1984e1d6d1a51ff66bdefef224a1d6db9c24a51
Logan Lampton <lampton.logan@gmail.com>
2022-12-31 15:57:26 -0500

    added in spans for label letters

---
+++ form_wave_animation/script.js
@@ -0,0 +1,12 @@
+const labels = document.querySelectorAll('.form-control label')
+
+// can use forEach, since labels is a node list
+labels.forEach(label => {
+    label.innerHTML = label.innerText
+        // split the label into individual letters
+        .split('')
+        // run code on an array each of the letters individually
+        .map((letter, index) => `<span>${letter}</span>`)
+        // turns the array back into a string
+        .join('')
+});
