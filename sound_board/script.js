322c3ed4de509772380613c5086e62d4fe49a7ae
Logan Lampton <lampton.logan@gmail.com>
2022-12-31 16:39:57 -0500

    created spotSongs method

--- sound_board/script.js
+++ sound_board/script.js
@@ -3,6 +3,24 @@ const sounds = ['applause', 'boo', 'gasp', 'tada', 'victory', 'wrong'];
 sounds.forEach(sound => {
     const btn = document.createElement('button');
     btn.classList.add('btn');
+
     btn.innerText = sound;
+
+    btn.addEventListener('click', () => {
+        
+        stopSongs();
+        // a built in audio method for HTML is .play
+        document.getElementById(sound).play()
+    });
+
     document.getElementById('buttons').appendChild(btn);
 })
+
+function stopSongs(){
+    sounds.forEach(sound => {
+        const song = document.getElementById(sound);
+        // built-in pause audio method and reset song to 0
+        song.pause();
+        song.currentTime = 0;
+    })
+}
