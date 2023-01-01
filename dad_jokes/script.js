f82f857e4f641bd2784690be3549224d1c4b67dc
Logan Lampton <lampton.logan@gmail.com>
2023-01-01 16:56:29 -0500

    async await version of the GET request

--- dad_jokes/script.js
+++ dad_jokes/script.js
@@ -9,15 +9,32 @@ generateJoke();
 
 // using a JSON API
 // Using a GET fetch request
-function generateJoke() {
+
+
+// using .then
+// function generateJoke() {
+//     const config = {
+//         headers: {
+//             'Accept': 'application/json'
+//         }
+//     }
+//     fetch('https://icanhazdadjoke.com', config)
+//         .then(res => res.json())
+//         .then((data) => {
+//             jokeEl.innerHTML = data.joke
+//         });
+// }
+
+// async await version
+async function generateJoke() {
     const config = {
         headers: {
             'Accept': 'application/json'
         }
     }
-    fetch('https://icanhazdadjoke.com', config)
-        .then(res => res.json())
-        .then((data) => {
-            jokeEl.innerHTML = data.joke
-        });
+    const res = await fetch('https://icanhazdadjoke.com', config)
+
+    const data = await res.json();
+    // use the joke info in the API, thus ".joke"
+    jokeEl.innerHTML = data.joke;
 }
