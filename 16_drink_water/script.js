903b9ed3c66b8133be1c84c76382903bbc00cb15
Logan Lampton <lampton.logan@gmail.com>
2023-01-05 18:25:09 -0500

    added JS for big cup

--- 16_drink_water/script.js
+++ 16_drink_water/script.js
@@ -3,6 +3,8 @@ const liters = document.getElementById('liters');
 const percentage = document.getElementById('percentage');
 const remained = document.getElementById('remained');
 
+// updateBigCup();
+
 smallCups.forEach((cup, index) => {
     cup.addEventListener('click', () => highlightCups(index));
 })
@@ -21,4 +23,33 @@ function highlightCups(index){
             cup.classList.remove('full');
         }
     })
+    updateBigCup();
+}
+
+function updateBigCup(){
+    // get the number of full cups
+    const fullCups = document.querySelectorAll('.cup-small.full').length;
+    // get the number of total cups (we grabbed smallCups earlier at the top of the doc)
+    const totalCups = smallCups.length;
+
+    if(fullCups === 0){
+        percentage.style.visibility = 'hidden';
+        percentage.style.height = 0;
+    } else {
+        percentage.style.visibility = 'visible';
+        // multiply by height of cup (330px)
+        percentage.style.height = `${fullCups / totalCups * 330}px`;
+        percentage.innerText = `${fullCups / totalCups *100}%`;
+    }
+
+    // hide the remained if 100% full, or show the percentage remaining
+    if(fullCups === totalCups){
+        remained.style.visibility = 'hidden';
+        remained.style.height = 0;
+    } else {
+        remained.style.visibility = 'visible';
+        // make the liters class div innerText the percentage remaining in Liters
+        // 2 liters total - 250ml * the number of cups that are full / 1000 to show in percentage
+        liters.innerText = `${2 - (250 * fullCups / 1000)}L`;
+    }
 }
