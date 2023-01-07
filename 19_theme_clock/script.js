eb2a54e97c520ec2328068f296fa076e9ac4331b
Logan Lampton <lampton.logan@gmail.com>
2023-01-07 17:42:15 -0500

    added methods for grabbing the date/time and dividing into buckets; added movment to needles

--- 19_theme_clock/script.js
+++ 19_theme_clock/script.js
@@ -8,6 +8,7 @@ const toggleElement = document.querySelector('.toggle');
 const days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
 const months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
 
+// dark/light mode toggle
 toggleElement.addEventListener('click', (e) => {
     const html = document.querySelector('html');
     if(html.classList.contains('dark')) {
@@ -18,3 +19,30 @@ toggleElement.addEventListener('click', (e) => {
         e.target.innerHTML = 'Light mode';
     }
 })
+
+// use built in Date class to get the current date and time
+function setTime(){
+    const time = new Date();
+    const month = time.getMonth();
+    const day = time.getDay();
+    const hours = time.getHours();
+    const hoursForClock = hours % 12;
+    const minutes = time.getMinutes();
+    const seconds = time.getSeconds();
+
+    hourElement.style.transform = `translate(-50%, -100%) rotate(${scale(hoursForClock, 0, 11, 0, 360)}deg)`;
+    minuteElement.style.transform = `translate(-50%, -100%) rotate(${scale(minutes, 0, 59, 0, 360)}deg)`;
+    secondElement.style.transform = `translate(-50%, -100%) rotate(${scale(seconds, 0, 59, 0, 360)}deg)`;
+
+};
+
+// function to scale 0 to 11 (for 12 hours) to 0 to 360 (for degrees in the clock)
+const scale = (num, in_min, in_max, out_min, out_max) => {
+    return (num - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
+}
+
+setTime();
+
+// keep calling the setTime method on the interval below to make it a moving clock, rather than static after page load
+// 1000ms = 1 second
+setInterval(setTime, 1000);
