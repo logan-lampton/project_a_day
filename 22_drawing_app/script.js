8b6e104c0b8ecdbb7c90f4e2d80eaf840aa648b3
Logan Lampton <lampton.logan@gmail.com>
2023-01-09 17:44:27 -0500

    added mouse eventListeners to have the circle and line follow the user's mouse

--- 22_drawing_app/script.js
+++ 22_drawing_app/script.js
@@ -3,7 +3,39 @@ const canvas = document.getElementById('canvas');
 const ctx = canvas.getContext('2d');
 
 let size = 20;
+let isPressed = false;
 let color = 'black';
+let x;
+let y;
+
+canvas.addEventListener('mousedown', (e) => {
+    isPressed = true;
+    x = e.offsetX;
+    y = e.offsetY;
+
+    // console.log(isPressed, x, y);
+})
+
+canvas.addEventListener('mouseup', (e) => {
+    isPressed = false;
+    x = undefined;
+    y = undefined;
+
+    // console.log(isPressed, x, y);
+})
+
+canvas.addEventListener('mousemove', (e) => {
+    if(isPressed){
+        const x2 = e.offsetX;
+        const y2 = e.offsetY;
+        drawCircle(x2, y2);
+        drawLine(x, y, x2, y2);
+
+        x = x2;
+        y = y2;
+        // console.log(x2, y2);
+    }
+})
 
 function drawCircle(x, y){
     ctx.beginPath();
@@ -17,9 +49,10 @@ function drawLine(x1, y1, x2, y2){
     ctx.moveTo(x1, y1);
     ctx.lineTo(x2, y2);
     ctx.strokeStyle = color;
-    ctx.lineWidth = size;
+    // want lineWidth * 2, so that the line is the width of the radius of the circle
+    ctx.lineWidth = size * 2;
     ctx.stroke();
 };
 
-drawCircle(400, 400);
-drawLine(250, 250, 500, 500);
+// drawCircle(400, 400);
+// drawLine(250, 250, 500, 500);
